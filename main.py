24001befc5c4bd92ede0ad3e90917ddd3fcae66b
Madison Simpson <madison_simpson@outlook.com>
2024-03-03 21:38:57 -0500

    point 308 complete

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
-
-
 quiz = QuizBrain(question_bank)
 
 quiz_ui = QuizInterface(quiz)
